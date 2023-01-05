cebff88032641418cbf5ff84339b094095114ee2
JC <jonnycromwell@yahoo.com>
2023-01-05 14:46:32 -0500

    Update to the Python file and the renamed the JSON file. Also, added the specific columns for the dataframe in the txt file

--- NHLplayerstats.py
+++ NHLplayerstats.py
@@ -10,7 +10,7 @@ stageJsonFile = 'stageNHLPlayers_' + statYear + '.json'
 apiKey = '8e39f5323e0a4e9f8b61e6ff35e16a85'
 url = 'https://api.sportsdata.io/v3/nhl/stats/json/PlayerSeasonStats/2022?key=' + apiKey
 
-
+# API Call Function
 def api_Call():
     # Call API with key
     response = requests.get(url)
@@ -26,10 +26,9 @@ def api_Call():
     jsonFile.write(jsonString)
     jsonFile.close()
 
-# API Call Function
-
 
 # Filtered json columns
-
+df = pd.read_json(apiJsonFile)
+print(df[["StatID","TeamID","PlayerID","SeasonType","Season","Name","Team","Position","GlobalTeamID","Games","Minutes","Seconds","Goals","Assists","ShotsOnGoal","PowerPlayGoals","ShortHandedGoals","EmptyNetGoals","PowerPlayAssists","ShortHandedAssists","HatTricks","ShootoutGoals","PlusMinus","PenaltyMinutes","Blocks","Hits","Takeaways","Giveaways","FaceoffsWon","FaceoffsLost","Shifts","GoaltendingMinutes","GoaltendingSeconds","GoaltendingShotsAgainst","GoaltendingGoalsAgainst","GoaltendingSaves","GoaltendingWins","GoaltendingLosses","GoaltendingShutouts","Started","BenchPenaltyMinutes","GoaltendingOvertimeLosses"]])
 
 # Connect to DB and load Staging
