9c4fc4898666ecffe8e34ac4d6cf74d8a5b09162
JC <jonnycromwell@yahoo.com>
2023-01-11 20:08:02 -0500

    Added SQLite Connection to NHLplayerstats.py with data insert from SportsData.io (as json) into NHL_stats.db

--- NHLplayerstats.py
+++ NHLplayerstats.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import pandas as pd
+import sqlite3
 
 # Create variables
 statYear = '2022'
@@ -30,7 +31,24 @@ def api_Call():
 api_Call()
 
 # Filtered json columns
-df = pd.read_json(apiJsonFile)
-print(df[["StatID","TeamID","PlayerID","SeasonType","Season","Name","Team","Position","GlobalTeamID","Games","Minutes","Seconds","Goals","Assists","ShotsOnGoal","PowerPlayGoals","ShortHandedGoals","EmptyNetGoals","PowerPlayAssists","ShortHandedAssists","HatTricks","ShootoutGoals","PlusMinus","PenaltyMinutes","Blocks","Hits","Takeaways","Giveaways","FaceoffsWon","FaceoffsLost","Shifts","GoaltendingMinutes","GoaltendingSeconds","GoaltendingShotsAgainst","GoaltendingGoalsAgainst","GoaltendingSaves","GoaltendingWins","GoaltendingLosses","GoaltendingShutouts","Started","BenchPenaltyMinutes","GoaltendingOvertimeLosses"]])
+#df = pd.read_json(apiJsonFile)
+#print(df[["StatID","TeamID","PlayerID","SeasonType","Season","Name","Team","Position","GlobalTeamID","Games","Minutes","Seconds","Goals","Assists","ShotsOnGoal","PowerPlayGoals","ShortHandedGoals","EmptyNetGoals","PowerPlayAssists","ShortHandedAssists","HatTricks","ShootoutGoals","PlusMinus","PenaltyMinutes","Blocks","Hits","Takeaways","Giveaways","FaceoffsWon","FaceoffsLost","Shifts","GoaltendingMinutes","GoaltendingSeconds","GoaltendingShotsAgainst","GoaltendingGoalsAgainst","GoaltendingSaves","GoaltendingWins","GoaltendingLosses","GoaltendingShutouts","Started","BenchPenaltyMinutes","GoaltendingOvertimeLosses"]])
 
 # Connect to DB and load Staging
+try:
+    sqliteConnection = sqlite3.connect('NHL_stats.db')
+    cursor = sqliteConnection.cursor()
+
+    df = pd.read_json(apiJsonFile)
+    stats = df[["StatID","TeamID","PlayerID","SeasonType","Season","Name","Team","Position","GlobalTeamID","Games","Minutes","Seconds","Goals","Assists","ShotsOnGoal","PowerPlayGoals","ShortHandedGoals","EmptyNetGoals","PowerPlayAssists","ShortHandedAssists","HatTricks","ShootoutGoals","PlusMinus","PenaltyMinutes","Blocks","Hits","Takeaways","Giveaways","FaceoffsWon","FaceoffsLost","Shifts","GoaltendingMinutes","GoaltendingSeconds","GoaltendingShotsAgainst","GoaltendingGoalsAgainst","GoaltendingSaves","GoaltendingWins","GoaltendingLosses","GoaltendingShutouts","Started","BenchPenaltyMinutes","GoaltendingOvertimeLosses"]]
+
+    stats.to_sql('stage_NHL_stats', sqliteConnection, if_exists = 'replace', index = False)
+    sqliteConnection.commit()
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
+finally:
+    if sqliteConnection:
+        sqliteConnection.close()
+        print("The SQLite connection is closed")
